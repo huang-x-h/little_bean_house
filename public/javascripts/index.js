f397b0d2e92b9bdb14fbdd6bc396be7376385803
huang.xinghui <huang.x.hui@gmail.com>
2015-05-13 13:26:33 +0800

    add baidu tongji

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,3 +6,12 @@ var timeline_config = {
     height: "100%",
     source: '/javascripts/little_bean_profile.json'
 }
+
+// baidu tongji
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?62d0cf1cf935cded759f51d28b5ccb74";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
