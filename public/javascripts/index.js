275cc1ea2847f9862585a9f4b69cf92c43af82b1
huang.xinghui <huang.x.hui@gmail.com>
2015-05-19 21:35:12 +0800

    add magnific lib

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,32 @@
 /**
  * Created by huangxinghui on 2015/5/2.
  */
-var timeline_config = {
-    width: "100%",
-    height: "100%",
-    source: '/javascripts/little_bean_profile.json'
-}
+
+$(document).ready(setTimeout(function() {
+    $.get('/javascripts/little_bean_profile.json').then(function (data) {
+        $('.media-image').magnificPopup({
+            items: data.timeline.date.map(function (item) {
+                return {
+                    src: item.asset.media,
+                    title: item.text
+                }
+            }),
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.title + '<small>by huangxinghui</small>';
+                }
+            }
+        });
+    });
+}, 2000));
 
 // baidu tongji
 var _hmt = _hmt || [];
