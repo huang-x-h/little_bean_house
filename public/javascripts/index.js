b1f782b50103560d17a7f4e543e9bf62f35d22de
huang.xinghui <huang.x.hui@gmail.com>
2015-05-20 09:58:31 +0800

    change initialize

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,35 +2,46 @@
  * Created by huangxinghui on 2015/5/2.
  */
 
-$(document).ready(setTimeout(function() {
+$(document).ready(function () {
     $.get('/javascripts/little_bean_profile.json').then(function (data) {
-        $('.media-image').magnificPopup({
-            items: data.timeline.date.map(function (item) {
-                return {
-                    src: item.asset.media,
-                    title: item.text
-                }
-            }).reverse(),
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true
-            },
-            image: {
-                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                titleSrc: function(item) {
-                    return item.title + '<small>by huangxinghui</small>';
-                }
-            }
+        createStoryJS({
+            type: 'timeline',
+            width: "100%",
+            height: "100%",
+            start_at_end: true,
+            source: data,
+            embed_id: 'timeline-embed'
         });
+
+        setTimeout(function () {
+            $('.media-image').magnificPopup({
+                items: data.timeline.date.map(function (item) {
+                    return {
+                        src: item.asset.media,
+                        title: item.text
+                    }
+                }).reverse(),
+                type: 'image',
+                tLoading: 'Loading image #%curr%...',
+                mainClass: 'mfp-img-mobile',
+                gallery: {
+                    enabled: true,
+                    navigateByImgClick: true
+                },
+                image: {
+                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                    titleSrc: function (item) {
+                        return item.title + '<small>by huangxinghui</small>';
+                    }
+                }
+            });
+        }, 2000);
     });
-}, 2000));
+});
 
 // baidu tongji
 var _hmt = _hmt || [];
-(function() {
+(function () {
     var hm = document.createElement("script");
     hm.src = "//hm.baidu.com/hm.js?62d0cf1cf935cded759f51d28b5ccb74";
     var s = document.getElementsByTagName("script")[0];
