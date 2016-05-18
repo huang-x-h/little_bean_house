724d46c325b65277e1254f9c553b83f79ef3540d
huang.xinghui <huang.x.hui@gmail.com>
2016-05-18 22:35:05 +0800

    modify image sort

--- scripts/profile.js
+++ scripts/profile.js
@@ -3,14 +3,13 @@
  */
 
 var fs = require('fs');
-var moment = require('moment');
 var profile = require('../public/javascripts/little_bean_profile.json');
 
 function refreshProfile(data) {
   var result = profile.timeline.date.concat(data);
 
   result = result.sort(function(a, b) {
-    if (moment(a.startDate, 'YYYY,M,D').valueOf() > moment(b.startDate, 'YYYY,M,D').valueOf()) {
+    if (a.asset.media > b.asset.media) {
       return 1;
     }
 
@@ -19,12 +18,14 @@ function refreshProfile(data) {
 
   profile.timeline.date = result;
 
-  fs.writeFile('./public/javascripts/little_bean_profile.json', JSON.stringify(profile), function(err) {
+  fs.writeFile('../public/javascripts/little_bean_profile.json', JSON.stringify(profile), function(err) {
     if (err) console.error(err);
 
     console.log('Profile refresh success.');
   })
 }
+
+refreshProfile();
 
 module.exports = {
   refreshProfile: refreshProfile
